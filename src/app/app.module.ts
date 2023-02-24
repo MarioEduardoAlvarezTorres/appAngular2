8ddc88ab813bd49123893396dbdf874e686140a1
Mario Eduardo <eduardoat44@gmail.com>
2023-02-24 10:47:10 -0600

    Creacion del servicio heroe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroesService } from './services/heroes.service';
 
 @NgModule({
   declarations: [
@@ -19,9 +20,11 @@ import { HeroesComponent } from './components/heroes/heroes.component';
   ],
   imports: [
     BrowserModule,
-    APP_ROUTING
+    APP_ROUTING,
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { HeroesService,Heroe } from "../../services/heroes.service";
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
 })
-export class HeroesComponent {
+export class HeroesComponent{
 
+  heroes:Heroe[] = [];
+
+  constructor( private _heroesService:HeroesService) {
+
+  }
+  ngOnInit(){
+    this.heroes = this._heroesService.getHeroes();
+  }
 }
