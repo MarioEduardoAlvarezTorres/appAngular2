116600c572060bbed76d6782019b4e43092de151
Mario Eduardo <eduardoat44@gmail.com>
2023-02-24 11:12:27 -0600

    uso de rutas con parametros

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HeroesService,Heroe } from "../../services/heroes.service";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -9,10 +10,14 @@ export class HeroesComponent{
 
   heroes:Heroe[] = [];
 
-  constructor( private _heroesService:HeroesService) {
+  constructor( private _heroesService:HeroesService, private router:Router) {
 
   }
   ngOnInit(){
     this.heroes = this._heroesService.getHeroes();
+  }
+
+  verHeroe(idx:number) {
+    this.router.navigate(['/heroe',idx])
   }
 }
