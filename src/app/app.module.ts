3284904ed23c1e6104e88f937a2e572962472024
Mario Eduardo <eduardoat44@gmail.com>
2023-02-24 12:35:13 -0600

    Se agrega funcionalidad a buscar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroesService } from './services/heroes.service';
 import { HeroeComponent } from './components/heroe/heroe.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,8 @@ import { HeroeComponent } from './components/heroe/heroe.component';
   imports: [
     BrowserModule,
     APP_ROUTING,
+    BrowserModule,    
+    FormsModule
   ],
   providers: [
     HeroesService
